b4efa56884136c268d715779d196f774a1dff8d3
George_N <georgenabilawad@gmail.com>
2025-06-04 22:37:56 +0300

    second commit

--- Basketball-Points-Counter/lib/main.dart
+++ Basketball-Points-Counter/lib/main.dart
@@ -65,7 +65,7 @@ class _pointsCounterState extends State<PointsCounter> {
                         backgroundColor: Colors.orange,
                         minimumSize: Size(140, 50),
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
+                            borderRadius: BorderRadius.circular(9)),
                       ),
                       onPressed: () {
                         setState(() {
